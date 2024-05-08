ec1d1d920aba03d54ccc8819323fc06dbeef3db8
hoangnam111b <so0o0l2010pun0suy@hotmail.com>
2024-05-08 10:03:48 +0700

    update task

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-function maximalRectangle(matrix) {
-  if (matrix.length === 0 || matrix[0].length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const heights = new Array(cols).fill(0);
-  let maxArea = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      heights[j] = matrix[i][j] === "1" ? heights[j] + 1 : 0;
-    }
-    maxArea = Math.max(maxArea, largestRectangleArea(heights));
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
   }
-  return maxArea;
-  function largestRectangleArea(heights) {
-    const stack = [];
-    let maxArea = 0;
-    for (let i = 0; i <= heights.length; i++) {
-      while (
-        stack.length &&
-        (i === heights.length || heights[i] < heights[stack[stack.length - 1]])
-      ) {
-        const height = heights[stack.pop()];
-        const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
-        maxArea = Math.max(maxArea, height * width);
-      }
-      stack.push(i);
-    }
-    return maxArea;
-  }
+  return maxProfit;
 }
