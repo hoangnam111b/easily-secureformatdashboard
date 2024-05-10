08a975d9e614d8839872bbdb51aee1f56a74d89c
hoangnam111b <so0o0l2010pun0suy@hotmail.com>
2024-05-10 16:34:20 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-    arr.slice(i * size, i * size + size),
-  );
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) left.push(arr[i]);
+    else if (arr[i] > pivot) right.push(arr[i]);
+  }
+  return quickSort(left).concat([pivot]).concat(quickSort(right));
+}
